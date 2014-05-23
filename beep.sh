c66cfacb83de67b33ae730a652f0d92fe7d02b6a
1kfavs <100favs@gmail.com>
2014-05-23 22:16:29 +0900

    initial release

---
+++ beep.sh
@@ -0,0 +1,83 @@
+#! /bin/bash
+echo 清掃中......
+
+# 1 テレレレレレレー
+beep -f 391 -l 200
+beep -f 466 -l 200
+beep -f 622 -l 200
+beep -f 782 -l 200
+beep -f 932 -l 200
+beep -f 1244 -l 200
+beep -f 1564 -l 1200
+beep -f 1396 -l 200
+
+# 2 テテーレレレレレー
+beep -f 1396 -l 400
+beep -f 1244 -l 200
+beep -f 1244 -l 200
+beep -f 1174 -l 200
+beep -f 1046 -l 200
+beep -f 1046 -l 1200
+
+# 3 テレレレレレレー
+beep -f 233 -l 200
+beep -f 293 -l 200
+beep -f 349 -l 200
+beep -f 466 -l 200
+beep -f 586 -l 200
+beep -f 698 -l 200
+beep -f 1174 -l 1200
+
+
+# 4 テテーレレレレレー
+beep -f 1046 -l 200
+beep -f 1046 -l 400
+beep -f 932 -l 200
+beep -f 932 -l 200
+beep -f 830 -l 200
+beep -f 783 -l 200
+beep -f 783 -l 1200
+
+
+# 3 テレレレレレレー
+beep -f 391 -l 200
+beep -f 466 -l 200
+beep -f 622 -l 200
+beep -f 782 -l 200
+beep -f 932 -l 200
+beep -f 1244 -l 200
+beep -f 1564 -l 1200
+
+# 4 テレーレレレレレー
+beep -f 1396 -l 200
+beep -f 1396 -l 400
+beep -f 1244 -l 200
+beep -f 1244 -l 200
+beep -f 1174 -l 200
+beep -f 1046 -l 200
+beep -f 1046 -l 1200
+
+# 5 テレレレレレ
+beep -f 293 -l 200
+beep -f 349 -l 200
+beep -f 415 -l 200
+beep -f 466 -l 200
+beep -f 586 -l 200
+beep -f 698 -l 200
+
+
+# 6 テレレレレレ
+beep -f 391 -l 200
+beep -f 466 -l 200
+beep -f 622 -l 200
+beep -f 782 -l 200
+beep -f 932 -l 200
+beep -f 1244 -l 200
+
+# 7 テレレレーレレー
+beep -f 932 -l 200
+beep -f 880 -l 200
+beep -f 932 -l 200
+beep -f 1564 -l 400
+beep -f 1396 -l 200
+beep -f 1244 -l 800
