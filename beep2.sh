3e71ce8e364de098b23698a1905b84b907f40333
1kfavs <100favs@gmail.com>
2014-05-23 22:28:25 +0900

    後é音の追加

---
+++ beep2.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
+beep -f 880 -l 800
+sleep 0.5
