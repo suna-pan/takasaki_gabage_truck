b54bb246ec41e6e3ecd56ffac91ed91107f2285c
suna_pan <lisknkknk.al.g2k13@gmail.com>
2014-05-24 18:04:53 +0900

    楽譜に合わせた
    周波数,長さを変数化

--- beep.sh
+++ beep.sh
@@ -7,130 +7,153 @@ fi
 
 modprobe pcspkr
 
+# note length
+n2=1400
+n4l=1050
+n4=700
+n8l=525
+n8=350
+n83=233
+n16=175
+
+# note freq
+As3=233
+D4=294
+Ds4=311
+F4=349
+G4=392
+Gs4=415
+As4=466
+D5=587
+Ds5=622
+F5=698
+G5=784
+Gs5=831
+A5=880
+As5=932
+C6=1047
+D6=1175
+Ds6=1245
+F6=1397
+G6=1568
+
 echo 清掃中......
 
 # 1 テレレレレレレー
-beep -f 391 -l 300
-beep -f 466 -l 300
-beep -f 622 -l 300
-beep -f 782 -l 300
-beep -f 932 -l 300
-beep -f 1244 -l 300
-beep -f 1564 -l 1200
-sleep 0.6
-beep -f 1396 -l 300
+beep -f $G4 -l $n83
+beep -f $As4 -l $n83
+beep -f $Ds5 -l $n83
+beep -f $G5 -l $n83
+beep -f $As5 -l $n83
+beep -f $Ds6 -l $n83
+beep -f $G6 -l $n4l
 
 # 2 テーレレレレレー
-beep -f 1396 -l 600
-beep -f 1244 -l 300
-beep -f 1244 -l 300
-beep -f 1174 -l 300
-beep -f 1046 -l 300
-beep -f 1046 -l 1200
-sleep 0.6
+beep -f $F6 -l $n8
+beep -f $F6 -l $n8l
+beep -f $Ds6 -l $n16
+beep -f $Ds6 -l $n83
+beep -f $D6 -l $n83
+beep -f $C6 -l $n83
+beep -f $C6 -l $n4
+sleep 0.$n4
 
 # 3 テレレレレレレー
-beep -f 233 -l 300
-beep -f 293 -l 300
-beep -f 349 -l 300
-beep -f 466 -l 300
-beep -f 586 -l 300
-beep -f 698 -l 300
-beep -f 1174 -l 1200
-sleep 0.6
-
+beep -f $As3 -l $n83
+beep -f $D4 -l $n83
+beep -f $F4 -l $n83
+beep -f $As4 -l $n83
+beep -f $D5 -l $n83
+beep -f $F5 -l $n83
+beep -f $D6 -l $n4l
 
 # 4 テテーレレレレレー
-beep -f 1046 -l 300
-beep -f 1046 -l 600
-beep -f 932 -l 300
-beep -f 932 -l 300
-beep -f 830 -l 300
-beep -f 783 -l 300
-beep -f 783 -l 1200
-
-sleep 0.6
+beep -f $C6 -l $n8
+beep -f $C6 -l $n8l
+beep -f $As5 -l $n16
+beep -f $As5 -l $n83
+beep -f $Gs5 -l $n83
+beep -f $G5 -l $n83
+beep -f $G5 -l $n4
+sleep 0.$n4
 
 # 3 テレレレレレレー
-beep -f 391 -l 300
-beep -f 466 -l 300
-beep -f 622 -l 300
-beep -f 782 -l 300
-beep -f 932 -l 300
-beep -f 1244 -l 300
-beep -f 1564 -l 1200
+beep -f $G4 -l $n83
+beep -f $As4 -l $n83
+beep -f $Ds5 -l $n83
+beep -f $G5 -l $n83
+beep -f $As5 -l $n83
+beep -f $Ds6 -l $n83
+beep -f $G6 -l $n4l
 
-sleep 0.6
 # 4 テレーレレレレレー
-beep -f 1396 -l 300
-beep -f 1396 -l 600
-beep -f 1244 -l 300
-beep -f 1244 -l 300
-beep -f 1174 -l 300
-beep -f 1046 -l 300
-beep -f 1046 -l 1200
-sleep 0.6
+beep -f $F6 -l $n8
+beep -f $F6 -l $n8l
+beep -f $Ds6 -l $n16
+beep -f $Ds6 -l $n83
+beep -f $D6 -l $n83
+beep -f $C6 -l $n83
+beep -f $C6 -l $n4
+sleep 0.$n4
+
 # 5 テレレレレレ テレレレレレ
-beep -f 293 -l 300
-beep -f 349 -l 300
-beep -f 415 -l 300
-beep -f 466 -l 300
-beep -f 586 -l 300
-beep -f 698 -l 300
+beep -f $D4 -l $n83
+beep -f $F4 -l $n83
+beep -f $Gs4 -l $n83
+beep -f $As4 -l $n83
+beep -f $D5 -l $n83
+beep -f $F5 -l $n83
 
-
-beep -f 391 -l 300
-beep -f 466 -l 300
-beep -f 622 -l 300
-beep -f 782 -l 300
-beep -f 932 -l 300
-beep -f 1244 -l 300
+beep -f $G4 -l $n83
+beep -f $As4 -l $n83
+beep -f $Ds5 -l $n83
+beep -f $G5 -l $n83
+beep -f $As5 -l $n83
+beep -f $Ds6 -l $n83
 
 # 7 テレレレーレレー
-beep -f 932 -l 300
-beep -f 880 -l 300
-beep -f 932 -l 300
-beep -f 1564 -l 600
-beep -f 1396 -l 300
-beep -f 1244 -l 1200
-
+beep -f $As5 -l $n83
+beep -f $A5 -l $n83
+beep -f $As5 -l $n83
+beep -f $G6 -l $n8l
+beep -f $F6 -l $n16
+beep -f $Ds6 -l $n4
+sleep 0.$n4
 
 
 # 1 テレレレレレレー
-beep -f 466 -l 300
-beep -f 391 -l 300
-beep -f 466 -l 300
-beep -f 622 -l 300
-beep -f 782 -l 300
-beep -f 932 -l 300
-beep -f 1564 -l 1200
-sleep 0.6
-beep -f 1396 -l 300
+beep -f $As4 -l $n83
+beep -f $G4 -l $n83
+beep -f $As4 -l $n83
+beep -f $Ds5 -l $n83
+beep -f $G5 -l $n83
+beep -f $As5 -l $n83
+beep -f $G6 -l $n4l
 
 # 2 テーレレレレレー
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1396 -l 50
-beep -f 1244 -l 50
-beep -f 1244 -l 300
-beep -f 1174 -l 300
-beep -f 1046 -l 300
-beep -f 1046 -l 1200
-sleep 0.6
+beep -f $F6 -l $n83
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $F6 -l 50
+beep -f $Ds6 -l 50
+beep -f $Ds6 -l $n83
+beep -f $D6 -l $n83
+beep -f $C6 -l $n83
+beep -f $C6 -l $n4l
 
 
 modprobe -rdd pcspkr
